dd5a05cc01290e4bd0f670fd496c17b3535879bd
alaminzuama <94155404+alaminzuama@users.noreply.github.com>
2021-11-17 19:13:03 +0700

    Update Identitas saya.py

--- Identitas saya.py
+++ Identitas saya.py
@@ -1,14 +1,14 @@
 #PERKENALAN SAYA
 print(20*("="),"Perkenalan",20*("="))
 Nama = "MUHAMMAD ZUAMA AL AMIN"
-nim = 212410102032
+Nim = 212410102032
 Umur = "18 tahun"
 Hobi = "Olahraga"
 Tinggi_Badan = "176 cm"
 Berat_badan= "73 kg"
 
 print("Nama             =",Nama)
-print("Nim              =",nim)
+print("Nim              =",Nim)
 print("Umur             =",Umur)
 print("Hobi             =",Hobi)
 print("Tinggi Badan     =",Tinggi_Badan)
